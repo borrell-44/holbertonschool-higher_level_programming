46a3ac12aaac8c3536797a970793fc3ff1551d2a
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-26 17:18:40 +0000

    Eliminating tabs

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 import random
 import sys
 number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
 sys.stdout.write("Last digit of {0} is {1} ".format(number, last))
 if last > 5:
-	print("and is greater than 5")
+ print("and is greater than 5")
 elif last == 0:
-	print("and is 0")
+ print("and is 0")
 else:
-	print("and is less than 6 and not 0")
+ print("and is less than 6 and not 0")
