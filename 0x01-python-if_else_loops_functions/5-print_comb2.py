87974002cb4b88a03d06512652e1883fdc564f12
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-26 19:17:29 +0000

    Putting a condition inside the print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,4 @@ for i in range(0, 100):
     if i < 10:
         print("{:d}".format(0), end="")
     print("{:d}".format(i), end="")
-    if i != 99:
-        print(", ", end="")
-    else:
-        print()
+    print(", " if i != 99 else "\n", end="")
