cfc53bdfc66a0481a61f2959b0c9402bbf018291
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-26 20:52:19 +0000

    Commiting task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    l = len(str)
+    for i in range(0, l):
+        c = str[i]
+        if ord(c) >= 97 and ord(c) <= 122:
+            u = chr(ord(c) - 32)
+            print("{}".format(u) if i != l-1 else "{}\n".format(u), end="")
+        else:
+            print("{}".format(c) if i != l-1 else "{}\n".format(u), end="")
+
