414eb099b1f9786e7fcb5735ab61b9b31d3a6bc1
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-26 17:52:22 +0000

    Fixing indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 last = int(repr(number)[-1])
 print("Last digit of {0} is ".format(number), end="")
 if number < 0 and last != 0:
- print("-{0} and is less than 6 and not 0".format(last))
+    print("-{0} and is less than 6 and not 0".format(last))
 elif last < 0 and last < 6:
- print("{0} and is less than 6 and not 0".format(last))
+    print("{0} and is less than 6 and not 0".format(last))
 elif last == 0:
- print("{0} and is 0".format(last))
+    print("{0} and is 0".format(last))
 else:
- print("{0} and is greater than 5".format(last))
+    print("{0} and is greater than 5".format(last))
