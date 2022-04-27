d844428975d4d938027af7d38c3bedce9d39e4c9
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-27 03:08:40 +0000

    Fixing minor bugs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 num = 32
 for i in range(90, 64, -1):
-    print("{}".format(chr(i + num)) if num > 0 else "{}".format(chr(i)), end="")
+    u = chr(i + num)
+    l = chr(i)
+    print("{}".format(u) if num > 0 else "{}".format(l), end="")
     num *= -1
-print()
