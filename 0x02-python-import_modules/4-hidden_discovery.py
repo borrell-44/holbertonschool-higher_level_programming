19299295e5e27d9fda65d9a484f00c26417ce578
borrell-44 <alexanderborrell44@hotmail.com>
2022-04-28 21:22:40 +0000

    Adding the python coding style

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 import hidden_4
 if __name__ == "__main__":
     for i in range(0, len(dir(hidden_4))):
-        if dir(hidden_4)[i][0] != '_': 
+        if dir(hidden_4)[i][0] != '_':
             print(dir(hidden_4)[i])
