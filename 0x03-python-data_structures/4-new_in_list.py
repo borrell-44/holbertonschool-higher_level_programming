938952c8521dd4f7e87dd7a91a4210b14e8a1192
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-02 16:45:58 +0000

    Hot Fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     copy = []
     for i in range(0, len(my_list)):
         copy.append(my_list[i])
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return copy
     copy[idx] = element
     return copy
