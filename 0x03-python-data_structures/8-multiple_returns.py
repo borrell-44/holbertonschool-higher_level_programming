4385b4a7affd7c18b067b21fd47ffb82b64c8486
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-02 20:11:43 +0000

    Hot Fix

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tup = (0, None)
+    if not sentence:
+        return tup
+    lis = list(tup)
+    lis[1] = sentences[0]
+    lis[0] = len(sentence)
+    tup = tuple(lis)
+    return tup
