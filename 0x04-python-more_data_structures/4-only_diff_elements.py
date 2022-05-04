179c84a06517f2fe2ec3eefd1483de630c7aa97b
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-04 19:18:52 +0000

    Making the code work with empty libraries

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,11 +2,12 @@
 def only_diff_elements(set_1, set_2):
     uncommon = []
 
-    if set_1 and set_2:
+    if set_1:
         for i in set_1:
             if i not in set_2:
                 uncommon.append(i)
 
+    if set_2:
         for j in set_2:
             if j not in set_1:
                 uncommon.append(j)
