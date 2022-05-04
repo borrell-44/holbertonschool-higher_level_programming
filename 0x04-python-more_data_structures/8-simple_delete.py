cf562d7e8c6f0773a15c1ec84f38bcacfb876714
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-04 20:30:39 +0000

    Adding a return value to the function

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, keys=""):
     if a_dictionary:
-        if keys in a_dictionary:
+        if keys in a_dictionary.keys():
             del a_dictionary[keys]
+    return a_dictionary
