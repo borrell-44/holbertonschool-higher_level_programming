51888d6b2b926dcd53570cc3741870d386081f82
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-16 19:02:54 +0000

    Printing the exception when an integer is not given

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
+    import traceback
+
     try:
         print("{:d}".format(value))
     except ValueError:
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
         return False
 
     return True
