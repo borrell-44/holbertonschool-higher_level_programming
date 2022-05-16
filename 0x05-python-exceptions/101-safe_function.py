fa893e283ac250c5a35e482e7ae02fab57b3b066
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-16 20:53:41 +0000

    Hot Fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     import sys
+    end = []
 
     try:
-        return fct
-    except ZeroDivisionError:
+        end = fct(args[0], args[1])
+    except (IndexError, ZeroDivisionError):
         print("Exception:", sys.exc_info()[1], file=sys.stderr)
         return None
 
