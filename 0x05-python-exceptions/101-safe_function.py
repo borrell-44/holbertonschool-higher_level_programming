f86c0187716b16da4efd698cd4fe0de4b5d4dd0e
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-16 20:26:32 +0000

    Committing task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+
+    try:
+        return fct
+    except ZeroDivisionError:
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
+        return None
+
+    return end
