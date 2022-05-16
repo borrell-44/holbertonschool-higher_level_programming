3e40b9958e2bbe142ffe5f8a968a7c55051ef5fd
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-16 19:09:51 +0000

    Hot Fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
-    import traceback
 
     try:
         print("{:d}".format(value))
     except ValueError:
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
+        return False
+    except TypeError:
         print("Exception:", sys.exc_info()[1], file=sys.stderr)
         return False
 
