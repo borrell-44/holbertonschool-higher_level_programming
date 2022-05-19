a32d235365da38a9f1aa3a346a6854f8bf38b1cc
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-19 16:21:01 +0000

    Hot Fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
+""" File Comments """
+
+
 class Square:
+
+    """ class Square """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
