72957814df5327cb42f242f64fdfe349443c0980
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-25 13:42:41 +0000

    Adding comments

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+""" File Comments """
+
+
 def say_my_name(first_name, last_name=""):
+
+    """ function Comments """
+
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
