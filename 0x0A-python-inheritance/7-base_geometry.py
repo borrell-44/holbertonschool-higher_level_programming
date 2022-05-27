a63d0359857f76ff135d70ccef3d515fbd2538eb
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-27 20:31:10 +0000

    Committing task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+
+class BaseGeometry():
+
+    """ class Comments """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+
+        if value < 1:
+            raise ValueError(name + " must be greater than 0")
