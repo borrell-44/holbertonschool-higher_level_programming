150c5ac42d6403f487ce14e58810648068cffa83
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-31 15:17:21 +0000

    Committing task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+
+class MyInt(int):
+
+    """ class Comments """
+
+    def __eq__(self, value):
+        return False
+
+    def __ne__(self, value):
+        return True
