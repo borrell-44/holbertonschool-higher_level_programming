cadbd10a62cd7c2a8fd00dfeab813efecfd23c38
borrell-44 <alexanderborrell44@hotmail.com>
2022-05-31 15:53:13 +0000

    Committing task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+
+def add_attribute(cls, field, value):
+
+    """ function Commnets """
+
+    try:
+        setattr(cls, field, value)
+    except:
+        raise TypeError("can't add new attribute")
