f438af0adb9419f61a7fa5fefc971cd92d250549
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-02 16:34:27 +0000

    Committing task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+
+class Student(object):
+
+    """ class Comments """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        hold = self.__dict__
+        if attrs != None:
+            dic = {k: hold[k] for k in hold.keys() & attrs}
+            return dic
+        return hold
