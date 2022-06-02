429b19b8a574a415bda0b71254a259a41bdfb473
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-02 17:02:03 +0000

    Committing task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+
+class Student(object):
+
+    """ class Comments """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        hold = self.__dict__
+        if attrs is not None:
+            dic = {k: hold[k] for k in hold.keys() & attrs}
+            return dic
+        return hold
+
+    def reload_from_json(self, json):
+        for k in json:
+            setattr(self, k, json[k])
