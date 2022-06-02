f6b702b3caf816d9c7904f2834eabe31a75a90f0
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-02 18:03:02 +0000

    Documenting file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,16 +4,19 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+
+    """ function Comments """
+
     with open(filename, "r") as f:
         lines = f.readlines()
     lis = []
-    
-    for l in lines:
-        if search_string in l:
-            lis.append(l)
+
+    for lin in lines:
+        if search_string in lin:
+            lis.append(lin)
             lis.append(new_string)
         else:
-            lis.append(l)
+            lis.append(lin)
 
     str = ""
     for i in lis:
