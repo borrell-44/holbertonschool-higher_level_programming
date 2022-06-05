f900f8726a5a23b754f6d017b4a09081c62cae2d
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-05 14:25:05 +0000

    Committing new subclass Square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" file Comments """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    """ class Comments """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        string = "[Square] (" + str(self.id) + ") "
+        string += str(self.x) + "/" + str(self.y) + " - "
+        string += str(self.width)
+        return string
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        names = ["id", "size", "x", "y"]
+        if args:
+            for i in range(len(args)):
+                setattr(self, names[i], args[i])
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
