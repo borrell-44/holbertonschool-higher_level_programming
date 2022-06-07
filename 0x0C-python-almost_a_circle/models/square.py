f82ce7707160ad0e4ccdcc671158f8aa917887bf
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-07 20:40:26 +0000

    Addimg comments

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,10 +10,12 @@ class Square(Rectangle):
     """ class Comments """
 
     def __init__(self, size, x=0, y=0, id=None):
+        """ init constructure """
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
+        """ str constructure """
         string = "[Square] (" + str(self.id) + ") "
         string += str(self.x) + "/" + str(self.y) + " - "
         string += str(self.width)
@@ -21,14 +23,17 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """ size property """
         return self.width
 
     @size.setter
     def size(self, value):
+        """ size setter """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
+        """ update method """
         names = ["id", "size", "x", "y"]
         if args:
             for i in range(len(args)):
@@ -38,6 +43,7 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """ dictionary method """
         keys = ["id", "size", "x", "y"]
         values = [self.id, self.size, self.x, self.y]
         dic = {}
