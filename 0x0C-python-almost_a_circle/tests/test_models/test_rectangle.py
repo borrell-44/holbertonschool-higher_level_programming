8793b8975659405aad7bac5ebef559031157acea
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-08 16:15:59 +0000

    Tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,9 +9,12 @@ class TestRectangle(unittest.TestCase):
 
     """ class Comments """
 
-    def test_id(self):
-        r1 = Rectangle(10, 2)
-        self.assertTrue(r1.area())
+    def test_attr(self):
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        r1 = Rectangle(1, 2, 3)
+        self.assertEqual(r1.x, 3)
 
 if __name__ == '__main__':
     unittest.main()
