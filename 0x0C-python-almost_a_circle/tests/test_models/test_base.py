1cbd9bfeb9e964cd86f404ce8f4301218c6ebb48
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-08 11:58:49 +0000

    Adding a new test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -16,8 +16,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b2.id, 2)
         b3 = Base()
         self.assertEqual(b3.id, 3)
-        b4 = Base(12)
-        self.assertEqual(b4.id, 12)
+        b4 = Base(89)
+        self.assertEqual(b4.id, 89)
         b5 = Base()
         self.assertEqual(b5.id, 4)
 
