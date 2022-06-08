e82e412dd8ab61688308d60b3e56719158082fdf
borrell-44 <alexanderborrell44@hotmail.com>
2022-06-08 15:30:11 +0000

    Adding more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,6 +12,10 @@ class TestBase(unittest.TestCase):
     def test_auto_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(89)
+        self.assertEqual(b3.id, 89)
 
 
 if __name__ == '__main__':
