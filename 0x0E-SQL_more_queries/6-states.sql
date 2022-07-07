399c5d92032e01b3365d96c0e54075264c65a22b
borrell-44 <alexanderborrell44@hotmail.com>
2022-07-07 17:01:33 +0000

    Committing task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS states (
+	id int AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
+	name varchar(256) NOT NULL
+	);
