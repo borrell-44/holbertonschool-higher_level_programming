8cc3dcee819f287508520ec8d519ad40b5172e6e
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-01 19:08:56 -0400

    Committing task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+if (!isNaN(process.argv[2])) {
+  const num = parseInt(process.argv[2]);
+  console.log(fac(num));
+} else {
+  console.log(1);
+}
+
+function fac (a) {
+  if (a <= 1) {
+    return 1;
+  }
+  let num = 1;
+  for (let i = a; i > 0; i--) {
+    num = num * i;
+  }
+  return num;
+}
