b4e4f723c758ae4d9e4b54ba46e6a7b249d834f3
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-01 18:24:22 -0400

    Hot Fix

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (!isNaN(process.argv[2])) {
   const num = parseInt(process.argv[2]);
   let out = '';
-  for (let i = 1; i <= num && num > 0; i++) {
+  for (let i = 1; i <= num; i++) {
     for (let y = 0; y < num; y++) {
       out += 'X';
     }
