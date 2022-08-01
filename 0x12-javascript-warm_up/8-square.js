c06dd84d9401787e3ac954b07f1a957ed70cee65
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-01 18:13:07 -0400

    Committing task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+if (!isNaN(process.argv[2])) {
+  const num = parseInt(process.argv[2]);
+  let out = '';
+  for (let i = 1; i <= num && num > 0; i++) {
+    for (let y = 0; y < num; y++) {
+      out += 'X';
+    }
+    if (i !== num) {
+      out += '\n';
+    }
+  }
+  if (out !== '') {
+    console.log(out);
+  }
+} else {
+  console.log('Missing size');
+}
