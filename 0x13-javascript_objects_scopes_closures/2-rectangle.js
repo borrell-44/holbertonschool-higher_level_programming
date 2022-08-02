7c7c8b02ebddf3a720d8ce37997d0c855d2af054
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-02 11:03:22 -0400

    Committing task 3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if ((h <= 0 || !h) || (w <= 0 || !w)) {
-      return;
+      // pass
     } else {
-      console.log('%d --- %d', h, w)
       this.width = w;
       this.height = h;
     }
