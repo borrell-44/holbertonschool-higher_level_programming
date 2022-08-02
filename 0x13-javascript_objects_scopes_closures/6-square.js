d65ce7409fccc0b8460373d45a63e654f4713878
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-02 11:25:03 -0400

    Committing task 5 and 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let pr = 'c';
+    if (!c) {
+      pr = 'X';
+    }
+
+    let ch = '';
+    for (let i = 1; i <= this.height; i++) {
+      for (let j = 1; j <= this.width; j++) {
+        ch += pr;
+      }
+      if (i !== this.height) {
+        ch += '\n';
+      }
+    }
+    console.log(ch);
+  }
+};
