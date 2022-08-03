93de78f28e5d3ca66b1a2c57a4986819ac453258
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-03 08:26:59 -0400

    Testing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(process.argv.length);
+if (process.argv.length <= 3) {
+    console.log(0);
+}
+else {
+    const ar = process.argv.slice(2);
+    ar.sort();
+    console.log(parseInt(ar[ar.length - 2]));
+}
