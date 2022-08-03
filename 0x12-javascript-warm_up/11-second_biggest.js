0d43cac889917c4faef4d35fb4fffa78410ef622
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-03 08:43:48 -0400

    Hot Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-    console.log(0);
-}
-else {
-    const ar = process.argv.slice(2);
-    ar.sort();
-    console.log(parseInt(ar[ar.length - 2]));
+  console.log(0);
+} else {
+  const ar = process.argv.slice(2);
+  const index = ar.indexOf(Math.max.apply(null, ar));
+  ar.splice(index, 1);
+  console.log(Math.max.apply(null, ar));
 }
