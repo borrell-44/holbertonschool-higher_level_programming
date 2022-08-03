90bda5f8dd99df5d467fbfe77af3d9bb77bf63c3
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-03 09:00:39 -0400

    Hot Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,9 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const ar = process.argv.slice(2);
-  const index = ar.indexOf(Math.max.apply(null, ar));
-  ar.splice(index, 1);
+  if (ar.length > 2) {
+    const index = ar.indexOf(Math.max.apply(null, ar));
+    ar.splice(index, 1);
+  }
   console.log(Math.max.apply(null, ar));
 }
