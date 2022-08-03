c0f19c9515ff753faec9d583acdf8089788f9445
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-03 09:16:46 -0400

    It just works

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const ar = process.argv.slice(2);
-  const index = ar.indexOf(Math.max.apply(null, ar));
+  const index = ar.indexOf(Math.max.apply(null, ar).toString());
   ar.splice(index, 1);
   console.log(Math.max.apply(null, ar));
 }
