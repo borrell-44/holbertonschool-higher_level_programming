03597eaff8960b6a583798536e76a63edc189ad6
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-03 09:41:09 -0400

    Adding recursion to func

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-if (!isNaN(process.argv[2])) {
+
+function fac (num) {
+  if (num <= 1) {
+    return 1;
+  }
+  return num * (fac(num - 1));
+};
+
+if (process.argv.length <= 2) {
+  console.log(1);
+} else if (!isNaN(process.argv[2])) {
   const num = parseInt(process.argv[2]);
   console.log(fac(num));
 } else {
   console.log(1);
-}
-
-function fac (a) {
-  if (a <= 1) {
-    return 1;
-  }
-  let num = 1;
-  for (let i = a; i > 0; i--) {
-    num = num * i;
-  }
-  return num;
 }
