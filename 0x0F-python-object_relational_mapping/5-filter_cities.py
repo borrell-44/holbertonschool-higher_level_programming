e82c490f05e741ab4c43cca09577f069af6e667f
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-05 14:37:11 -0400

    Committing task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a script that lists all cities from the 
+database hbtn_0e_4_usa"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
+    c = db.cursor()
+
+    query = """SELECT cities.name
+    FROM cities, states
+    WHERE %s = states.name AND states.id = state_id
+    ORDER BY cities.id ASC"""
+    c.execute(query, (argv[4],))
+
+    rows = c.fetchall()
+    for i in range(len(rows)):
+        sys.stdout.write(rows[i][0])
+        if i != len(rows) - 1:
+            print(",", end=" ")
+        else:
+            print()
+
+    c.close()
+    db.close()
