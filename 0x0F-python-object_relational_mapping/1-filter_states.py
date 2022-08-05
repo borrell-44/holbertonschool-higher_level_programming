95562aacff9436317ca3f97e88faae2ddec38a22
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-05 10:16:52 -0400

    C

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Write a script that lists all states from the database hbtn_0e_0_usa"""
+"""Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+    c.execute("""SELECT * FROM states
+    WHERE name LIKE "N%" 
+    ORDER BY states.id ASC""")
     rows = c.fetchall()
     for row in rows:
         print(row)
