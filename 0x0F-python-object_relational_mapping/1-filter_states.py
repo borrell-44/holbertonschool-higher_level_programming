b9450af24b351e1d6979c733e3443644a5943c44
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-06 16:43:42 -0400

    edge case

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write script that lists all states with a 
+"""Write script that lists all states with a
 name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 if __name__ == "__main__":
