f44bf993a18b57d3a45eb6b67842f374de2c496f
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-06 16:50:35 -0400

    Testing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa where
-name matches the argument"""
+"""Once again, write a script that takes in arguments
+and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument. But
+this time, write one that is safe from MySQL injections!"""
 
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
     c = db.cursor()
-    query = f"""SELECT * FROM states
-    WHERE name = '{argv[4]}'
+
+    query = """SELECT * FROM states
+    WHERE name = %s
     ORDER BY states.id ASC"""
-    c.execute(query)
+    c.execute(query, (argv[4],))
+
     rows = c.fetchall()
     for row in rows:
         print(row)
+
     c.close()
     db.close()
