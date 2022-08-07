589a33a993f71e96669faf3bfb31186cc8f34399
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-07 14:39:21 -0400

    Committing task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all State objects
+from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+import sqlalchemy as db
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                              argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    meta = db.MetaData(bind=engine)
+    db.MetaData.reflect(meta)
+    states = meta.tables["states"]
+
+    update = db.update(states).where(states.columns.id
+                                     == 2).values(name="New Mexico")
+    engine.execute(update)
