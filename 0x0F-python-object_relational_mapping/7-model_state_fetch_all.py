02fe3bbb5f25dd161abbbd3650fd3306f933ecca
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-07 10:27:06 -0400

    Committing task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,27 +3,20 @@
 """Write a script that lists all State objects
 from the database hbtn_0e_6_usa"""
 
-
+from sys import argv
+from model_state import Base, State
+import sqlalchemy as db
 
 if __name__ == "__main__":
-    from sys import argv
-    import sys
-    import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], port=3306)
-    c = db.cursor()
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    st = State()
+    meta = db.MetaData()
+    con = engine.connect()
 
-    query = """SELECT cities.name
-    FROM cities, states
-    WHERE %s = states.name AND states.id = state_id
-    ORDER BY cities.id ASC"""
-    c.execute(query, (argv[4],))
-
-    rows = c.fetchall()
-    for i in range(len(rows)):
-        sys.stdout.write(rows[i][0])
-        if i != len(rows) - 1:
-            print(",", end=" ")
-    print()
-
-    c.close()
-    db.close()
+    census = db.Table('states', meta, autoload=True, autoload_with=engine)
+    query = db.select([census])
+    result = con.execute(query)
+    sets = result.fetchall()
+    for set in sets:
+        print("{}: {}".format(set[0], set[1]))
