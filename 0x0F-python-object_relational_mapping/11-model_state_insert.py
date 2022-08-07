3084fe622d59c76defbcbd44493e488df2275e63
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-07 14:19:39 -0400

    Committing task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all State objects
+from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+import sqlalchemy as db
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                              argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    meta = db.MetaData(bind=engine)
+    db.MetaData.reflect(meta)
+    states = meta.tables["states"]
+
+    query = db.select([db.func.max(states.columns.id)])
+    result = engine.execute(query).fetchall()
+
+    for row in result:
+        print(row[0])
