d5def08d9a83e162686716e5a1675525b8517e26
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-07 13:46:01 -0400

    Hot Fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,17 +11,16 @@ if __name__ == "__main__":
     engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                               argv[1], argv[2], argv[3]), pool_pre_ping=True)
     st = State()
-    bol = False
     meta = db.MetaData()
     con = engine.connect()
 
     census = db.Table('states', meta, autoload=True, autoload_with=engine)
-    query = db.select([census])
+    query = db.select(census).where(census.columns.name == argv[4])
     result = con.execute(query)
     sets = result.fetchall()
+
     for set in sets:
         if set[1] == argv[4]:
             print(set[0])
-            bol = True
-    if not bol:
+    if not sets:
         print("Not Found")
