49e89b5e082e430b7f00420eb71d5db8f5f67cf0
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-07 12:01:13 -0400

    Committing task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all State objects
+from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from model_state import Base, State
+import sqlalchemy as db
+
+if __name__ == "__main__":
+    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                              argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    st = State()
+    meta = db.MetaData()
+    con = engine.connect()
+
+    census = db.Table('states', meta, autoload=True, autoload_with=engine)
+    query = db.select([census]).where(census.columns.name ==
+                                      "{}".format(argv[4]))
+    result = con.execute(query)
+    sets = result.fetchall()
+    if not sets:
+        print("Not Found")
+    else:
+        for set in sets:
+            print(set[0])
