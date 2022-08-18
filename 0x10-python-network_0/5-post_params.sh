ad6bb5958aaa74f1696239f936bd61ef9506e547
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-18 16:12:53 -0400

    Hot Fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash                                                                                                                                                                               
 # Sends a POST request to the given URL and display body of the response                                                                                                                  
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -L -X POO                                                                                                         
-ST "$1" 
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -L -X POST "$1" 
