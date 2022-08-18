89746a15d6be3165f278e6daf75529d39633abb9
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-18 15:40:41 -0400

    Committing task 4

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash                                                                                                                                                                               
+# Sends a Get to for the given URL and display the body of the response                                                                                                                   
+curl -s -H "X-HolbertonSchool-User-Id: 98" -L "$1"
