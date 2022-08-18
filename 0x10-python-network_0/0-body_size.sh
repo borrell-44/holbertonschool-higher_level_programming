81507c185f7603adeb4603964ab335eabc53fc42
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-18 12:42:38 -0400

    Making the file executable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# Sends a request to the given URL and prints the size of the body response
+#Sends a request to the given URL and prints the size of the body response
 curl -s "$1" | wc -c
