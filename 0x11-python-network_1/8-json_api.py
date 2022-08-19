c112f55c3360357a3726bf979dd3679b4bd43ffd
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 16:29:00 -0400

    Committing task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fetches a URL with pythons requests library"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    letter = dict(q="")
+    if len(sys.argv) < 1:
+        letter = dict(q=sys.argv[1])
+
+    x = requests.post("http://0.0.0.0:5000/search_user", data=letter)
+    try:
+        response = x.json()
+    except ValueError:
+        print("Not a valid JSON")
+    if not response:
+        print("No result")
+    else:
+        print("[{}] {}".format(response.get('id'), response.get('name')))
