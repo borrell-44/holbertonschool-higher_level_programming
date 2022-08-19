455f2a6bbde517f224c319fcaf1d8a8c5e56255a
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 11:58:07 -0400

    Committing task 6

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fetches a URL with pythons requests library
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    x = requests.get(sys.argv[1])
+    if x.status_code >= 400:
+        print("Error code: {}".format(x.status_code))
+    else:
+        print(x.text)
