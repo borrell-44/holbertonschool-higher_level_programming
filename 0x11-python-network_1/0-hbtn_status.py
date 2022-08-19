b8c1956df5a69ec202a17c0d6928557ced38c712
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 12:27:52 -0400

    Hot Fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Fetches an URL and prints the response body
-
+"""Fetches an URL and prints the response body
+"""
 if __name__ == "__main__":
     # Print response body
     import urllib.request
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Sends a POST request to the given URL with the email as a parameter
+"""Sends a POST request to the given URL with the email as a parameter"""
 
 if __name__ == "__main__":
     # Comment
