20591d406885468e0fc4b344790dacafc2d3e81b
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 10:09:15 -0400

    Committing task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Sends an request for the given URL
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        body = response.__dict__['headers']
+        print(body.get('X-Request-Id'))
