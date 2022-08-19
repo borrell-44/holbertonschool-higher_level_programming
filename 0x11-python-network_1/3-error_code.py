c033ecd303e92446b2303c8de84d51fa7910fbd5
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 11:10:21 -0400

    Committing task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Sends a request to the given URL and displays the response body
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        err_num = e.code
+        print("Error code: {}".format(err_num))
