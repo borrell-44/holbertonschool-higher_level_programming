f271e7677fe5754cfd0309a2949bbe265b8d5db3
borrell-44 <alexanderborrell44@hotmail.com>
2022-08-19 11:26:41 -0400

    Committing task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Fetches a URL with pythons requests library
+
+if __name__ == "__main__":
+    import requests
+
+    x = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print(f"\t- type: {type(x.text)}")
+    print(f"\t- content: {x.text}")
