596ab2940420095122b7bd64d754468f4a470578
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-13 13:38:55 -0400

    Fixing syntax

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    console.log(data);
+  console.log(data);
 });
