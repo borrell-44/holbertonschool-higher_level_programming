5d5a686f4947f4920845c88d7433f60a21345fed
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-13 19:26:04 -0400

    Committing task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const fs = require('fs');
+const path = process.argv[2];
+const file = process.argv[3]
+
+axios.get(path).then(resp => {
+  fs.writeFile(file, resp.data, 'utf-8', (error) => {
+    if (error) console.log(err);
+  });
+
+}).catch(err => {
+  console.log(err);
+  return err;
+});
