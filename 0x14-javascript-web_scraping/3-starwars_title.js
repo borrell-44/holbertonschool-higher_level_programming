a45e3e4fd2b678f50d4ae761dbb401f21d8db443
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-13 15:59:15 -0400

    Fixing syntax

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const axios = require('axios');
-const path = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`
+const path = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
 axios.get(path).then(resp => {
-  var result = resp.data
-  console.log(result.title)
+  const result = resp.data;
+  console.log(result.title);
 }).catch(err => {
   console.log(err);
   return err;
