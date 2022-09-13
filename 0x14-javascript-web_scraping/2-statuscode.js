6b5409dae8944fe1098f6833e9b8bec2393a2a32
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-13 14:57:11 -0400

    Fixing syntax

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const axios = require('axios');
 axios.get(process.argv[2]).then(resp => {
   console.log(`code: ${resp.status}`);
 }).catch(err => {
-  console.log(`code: ${err['response']['status']}`);
+  console.log(`code: ${err.response.status}`);
   return err;
 });
