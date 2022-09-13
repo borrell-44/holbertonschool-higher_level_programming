18b40cfa0ebec13e8d86804c740cc2bb0f504dd6
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-13 19:28:36 -0400

    Formating the file to the standard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,13 +3,12 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = process.argv[2];
-const file = process.argv[3]
+const file = process.argv[3];
 
 axios.get(path).then(resp => {
   fs.writeFile(file, resp.data, 'utf-8', (error) => {
-    if (error) console.log(err);
+    if (error) console.log(error);
   });
-
 }).catch(err => {
   console.log(err);
   return err;
